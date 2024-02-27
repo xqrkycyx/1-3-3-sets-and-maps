41508d64b11fd78e13d20d25cebe90d41663b302
Will Williams <wwilliams@indeed.com>
2024-02-27 11:55:52 -0800

    Implements Set method intersect()

--- src/setOperations.js
+++ src/setOperations.js
@@ -17,7 +17,20 @@ function union(setA, setB) {
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
+// Function to compute the intersection of two Sets
+function intersect(setA, setB) {
+  // Create a new Set to store the intersection
+  const resultSet = new Set();
+
+  // Iterate through setA and check if each element is in setB
+  for (const elem of setA) {
+    if (setB.has(elem)) {
+      resultSet.add(elem);
+    }
+  }
+
+  return resultSet;
+}
 
 /**
  * return the difference of two sets
