91e8b0b77de1a0c91bd41bd6b6feaad826952270
Will Williams <wwilliams@indeed.com>
2024-02-27 11:56:15 -0800

    Implements Set method difference()

--- src/setOperations.js
+++ src/setOperations.js
@@ -35,6 +35,19 @@ function intersect(setA, setB) {
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+// Function to compute the difference of two Sets (elements in setA but not in setB)
+function difference(setA, setB) {
+  // Create a new Set to store the difference
+  const resultSet = new Set();
+
+  // Iterate through setA and add elements not in setB to resultSet
+  for (const elem of setA) {
+    if (!setB.has(elem)) {
+      resultSet.add(elem);
+    }
+  }
+
+  return resultSet;
+}
 
 module.exports = { union, intersect, difference };
